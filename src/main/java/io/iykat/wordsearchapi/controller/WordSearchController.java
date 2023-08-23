3afce7603abf1c07de722d3dc1fdb5b4e2fe10dc
prince takyi <takyip28@gmail.com>
2023-08-23 22:56:22 +0000

    Initial commit

---
+++ src/main/java/io/iykat/wordsearchapi/controller/WordSearchController.java
@@ -0,0 +1,38 @@
+package io.iykat.wordsearchapi.controller;
+
+import io.iykat.wordsearchapi.services.WordGridService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+@RequestMapping("/wordgrid")
+public class WordSearchController {
+
+    final
+    WordGridService wordGridService;
+
+    public WordSearchController(WordGridService wordGridService) {
+        this.wordGridService = wordGridService;
+    }
+
+    @GetMapping
+    @ResponseBody
+    public String createWordGrid(@RequestParam("gridSize") int gridSize, @RequestParam String wordList) {
+        System.out.println("WE ARE HERE");
+        List<String> words = Arrays.asList(wordList.split(","));
+        char[][] grid = wordGridService.generateGrid(gridSize, words);
+        String gridToString = "";
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
+                gridToString += grid[i][j] + " ";
+            }
+            gridToString += "\r\n";
+        }
+        return gridToString;
+    }
+
+}
